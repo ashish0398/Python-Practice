79b1977c986e77607ba295599767191f24d5f185
Ashish Singh <53008658+ashish0398@users.noreply.github.com>
2021-03-04 00:54:27 +0530

    1 file pushed

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
-a = 10  
-print("a =", a, end='\n\n\n')  
-print("a =", a, sep='0', end='$$$$$')
+# Function.
+""" num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+
+
+def largest(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        print("num1 is the largest")
+
+    if num2 > num1 and num2 > num3:
+        print("num2 is the largest")
+
+    if num3 > num1 and num3 > num2:
+        print("num3 is the largest")
+
+largest(num1, num2, num3) """
+
+# Loops
+# for loop with range
+""" n = int(input("Enter number: "))
+
+for i in range(1,11):
+    c = n*i
+    print(n,"*",i,"=",c) """
+
+# 
